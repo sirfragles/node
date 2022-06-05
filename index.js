14e04a881a1556a47b45abbbb36329002dfc1d9a
Matthew <sirfragles@gmail.com>
2022-06-05 16:23:12 +0200

    Deleted time

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + myDateTime());
   res.end('Hello World!');
 }).listen(8080);
-
-exports.myDateTime = function () {
-  return Date();
-};
